6321ceab6271a0975b9b42f2fa36c8fea333ed66
Niversdack <bb6bbe@gmail.com>
2020-08-21 18:57:11 +0500

    first commit

---
+++ internal/app/model/card.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+type Card struct {
+	ID        int
+	Name      string
+	ShortDesc string
+	FullDesc  string
+}
+
+// Validate ...
+func (c *Card) ValidateCard() error {
+	return validation.ValidateStruct(
+		c,
+		validation.Field(&c.Name, validation.Required),
+		validation.Field(&c.ShortDesc, validation.Required, validation.Length(10, 104)),
+		validation.Field(&c.FullDesc, validation.Required, validation.Length(10, 579)),
+	)
+}
